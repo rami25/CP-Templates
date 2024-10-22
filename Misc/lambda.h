bba35558aaf3c4f339f50dadcdbe0158c9364d33
rami25 <ramibenmrad4@gmail.com>
2024-10-22 17:54:49 +0100

    up

---
+++ Misc/lambda.h
@@ -0,0 +1,13 @@
+// lambda for recursive call
+
+template <class F> struct Function { 
+  F f;
+  Function(const F &f) : f(f) {} // NOLINT(*-explicit-constructor)
+
+  template <class... Args>
+  constexpr decltype(auto) operator()(Args &&... args) const {
+	  return f(*this, std::forward<Args>(args)...); 
+  }
+};
+
+template <class F> Function(const F &) -> Function<F>;
